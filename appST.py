9a7ed38031b2af6e2acfb1669819c84ca0013c7e
Richard Raul Mercedes Salazar <richardraul.mercedes@gmail.com>
2025-05-21 10:43:08 -0300

    Update appST.py

--- appST.py
+++ appST.py
@@ -1820,12 +1820,6 @@ def dms_to_decimal(dms):
     except Exception:
         return " "
 
-import streamlit as st
-
-# Simulación de la función dms_to_decimal
-def dms_to_decimal(dms_str):
-    return dms_str  # Simulación temporal
-
 def main():
     st.markdown(
     """
@@ -1869,7 +1863,7 @@ def main():
         Latitud_OP_DMS = st.text_input(f"Latitud - THR {Pista} [Grados Dec. sin (°) ó DMS sin (0)]",
                                       value="", placeholder="ex.: -34.906414/  345430.23S/  34°54'30.23''S", key="Latitud_OP_DMS")
     with col2:
-        Latitud_OP_decimal = dms_to_decimal(Latitud_OP_DMS)  # Convertir a decimal
+        Latitud_OP_decimal = dms_to_decimal(Latitud_OP_DMS)
         st.text_input("Latitud en Decimales", value=Latitud_OP_decimal, disabled=True, key="Latitud_OP_dec")
 
     # Entrada para longitud
@@ -1878,7 +1872,7 @@ def main():
         Longitud_OP_DMS = st.text_input(f"Longitud - THR {Pista} [Grados Dec. sin (°) ó  DMS sin (0)]",
                                        value="", placeholder="ex.: -57.943292/ 575630.91W/  57°56'30.91''W ", key="Longitud_OP_DMS")
     with col2:
-        Longitud_OP_decimal = dms_to_decimal(Longitud_OP_DMS)  # Convertir a decimal
+        Longitud_OP_decimal = dms_to_decimal(Longitud_OP_DMS)
         st.text_input("Longitud en Decimales", value=Longitud_OP_decimal, disabled=True, key="Longitud_OP_dec")
 
     Elevacion_OP = st.text_input(f"Elevación - THR {Pista} [m]", value="", key="Elevacion_OP", placeholder="ex.:  15.25 ")
@@ -1892,7 +1886,7 @@ def main():
         Latitud_EXT_DMS = st.text_input("Latitud - Ext. de Pista [Grados Dec. sin (°) ó  DMS sin (0)]",
                                        value="", placeholder="ex.: -34.906414/ 345430.23S/  34°54'30.23''S", key="Latitud_EXT_DMS")
     with col2:
-        Latitud_EXT_decimal = dms_to_decimal(Latitud_EXT_DMS)  # Convertir a decimal
+        Latitud_EXT_decimal = dms_to_decimal(Latitud_EXT_DMS)
         st.text_input("Latitud en Decimales", value=Latitud_EXT_decimal, disabled=True, key="Latitud_EXT_dec")
 
     # Entrada para longitud del extremo
@@ -1901,11 +1895,27 @@ def main():
         Longitud_EXT_DMS = st.text_input("Longitud - Ext. de Pista [Grados Dec. sin (°) ó  DMS sin (0)]",
                                         value="", placeholder="ex.: -57.943292/ 575630.91W/  57°56'30.91''W", key="Longitud_EXT_DMS")
     with col2:
-        Longitud_EXT_decimal = dms_to_decimal(Longitud_EXT_DMS)  # Convertir a decimal
+        Longitud_EXT_decimal = dms_to_decimal(Longitud_EXT_DMS)
         st.text_input("Longitud en Decimales", value=Longitud_EXT_decimal, disabled=True, key="Longitud_EXT_dec")
 
     Elevacion_EXT = st.text_input("Elevación - Extremo de Pista [m]", value="", key="Elevacion_EXT", placeholder="ex.:  15.25 ")
     st.divider()
+
+    # Botón para intercambiar datos
+    if st.button("Intercambiar Coordenadas y Elevación OP <-> EXT"):
+        temp_lat_dms = st.session_state["Latitud_OP_DMS"]
+        st.session_state["Latitud_OP_DMS"] = st.session_state["Latitud_EXT_DMS"]
+        st.session_state["Latitud_EXT_DMS"] = temp_lat_dms
+
+        temp_lon_dms = st.session_state["Longitud_OP_DMS"]
+        st.session_state["Longitud_OP_DMS"] = st.session_state["Longitud_EXT_DMS"]
+        st.session_state["Longitud_EXT_DMS"] = temp_lon_dms
+
+        temp_elev = st.session_state["Elevacion_OP"]
+        st.session_state["Elevacion_OP"] = st.session_state["Elevacion_EXT"]
+        st.session_state["Elevacion_EXT"] = temp_elev
+
+        st.experimental_rerun()
 
     st.markdown("__Datos Operativos__")
     t_aproximacion = st.selectbox('Tipo de Aproximación', ['Visual', 'No Precision', 'Precision CAT I', 'Precision CAT II o III'])
@@ -1913,7 +1923,6 @@ def main():
     ref_SHI = st.selectbox('Elevación de Referencia de la Superficie Horizontal Interna (SHI)', ['RWY - THR', 'Punto Medio', 'RWY - Extremo'])
     st.divider()
 
-    # Botón para generar el archivo
     if st.button("Generar .kml", key="boton_kml"):
         contenido = crear_genslo(nombre_ad, Pista, Longitud_OP_decimal, Latitud_OP_decimal,
                                  Elevacion_OP, Longitud_EXT_decimal, Latitud_EXT_decimal,
@@ -1972,8 +1981,6 @@ def main():
         mime="text/plain",
         key="descargar_informe"
     )
-    
 
 if __name__ == '__main__':
     main()
-
