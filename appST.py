7a712997d60f211a0ce982ad1ac70cc80f7274e8
Richard Raul Mercedes Salazar <richardraul.mercedes@gmail.com>
2025-05-23 09:50:15 -0300

    Update appST.py

--- appST.py
+++ appST.py
@@ -1875,7 +1875,7 @@ def main():
     # Entrada para latitud
     col1, col2 = st.columns(2)
     with col1:
-        st.markdown(f"**Latitud - THR {Pista} [Grados Dec. sin (°) ó DMS sin (0)]**")
+        #st.markdown(f"**Latitud - THR {Pista} [Grados Dec. sin (°) ó DMS sin (0)]**")
         Latitud_OP_DMS = st.text_input(label="", value="", placeholder="ex.: -34.906414 / 345430.23S / 34°54'30.23''S", key="Latitud_OP_DMS")
         
                                                                                                                                         #Latitud_OP_DMS = st.text_input(f"Latitud - THR {Pista} [Grados Dec. sin (°) ó DMS sin (0)]",
